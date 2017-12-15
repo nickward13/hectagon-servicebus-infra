115c575a9b78367ac9767f274033831032b5a084
Nick Ward <nickward@microsoft.com>
2017-12-15 11:38:24 +1100

    add environment parameter

--- azuredeploy.sh
+++ azuredeploy.sh
@@ -1,2 +1,13 @@
-az group create -n hectagon-servicebus -l australiasoutheast
-az group deployment create --name provision --resource-group hectagon-servicebus --template-file servicebus.json --parameters serviceBusNamespaceName=hectagon
+#!/bin/sh
+if [ $# -eq 0 ]
+  then
+    ENVIRONMENT=hectagon
+else
+    ENVIRONMENT=$1
+fi
+
+echo "Setting up environment '$ENVIRONMENT'..."
+RGNAME="$1-servicebus"
+az group create -n $RGNAME -l australiasoutheast
+export AZURE_SERVICEBUS_CONNECTION_STRING=$(az group deployment create --name provision --resource-group $RGNAME --template-file servicebus.json --parameters serviceBusNamespaceName=$ENVIRONMENT --query "properties.outputs.namespaceDefaultConnectionString.value" -o tsv)
+echo "Connection string is $AZURE_SERVICEBUS_CONNECTION_STRING"
